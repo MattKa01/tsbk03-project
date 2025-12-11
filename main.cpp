cf68ee2bc79cf4f7187c0194b9536af92561351c
MattKa01 <mattias@tailspin.se>
2025-12-11 13:08:14 +0100

    updated frag

--- main.cpp
+++ main.cpp
@@ -2,16 +2,24 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include "Shader.h"
+#include "glm/glm.hpp"
+#include "glm/ext.hpp"
 
 
 float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+    -1.0f,  1.0f,  0.0f,
+     1.0f,  1.0f,  0.0f,
+     1.0f, -1.0f,  0.0f,
+    -1.0f, -1.0f,  0.0f
+};
+GLuint indices[] = {
+    0, 1, 2,
+    2, 3, 0
 };
 
 GLuint VBO, VAO;
 GLFWwindow* window;
+glm::mat4 lookAt = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
 // Callback function for window resizing
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -62,7 +70,7 @@ void init() {
     // Set up VBO
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -82,7 +90,9 @@ void display(Shader shaders) {
 
         // Rendering commands here
         shaders.use();
-
+        int wHeight, wWidth;
+        glfwGetWindowSize(window, &wWidth, &wHeight);
+        glUniform2iv(glGetUniformLocation(shaders.ID, "Resolution"),2, (int[]){wWidth, wHeight});
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         // Check and call events and swap buffers
--- main.frag
+++ main.frag
@@ -1,7 +1,42 @@
 #version 330
 
+uniform vec2 Resolution;
+in vec2 outPos;
 out vec4 fragColor;
 
+// Simple height function for terrain
+float heightFunc(float x, float z) {
+    return sin(x) * sin(z);
+}
+
+// Ray marching function to find intersection with terrain
+bool castRay(vec3 rayOrigin, vec3 rayDir, out float resT) {
+    float minDist = 0.001f;
+    float maxDist = 10.0f;
+    float stepSize = 0.01f;
+
+    // Linear interpolation variables
+    float lh = 0.0;
+    float ly = 0.0;
+    for (float t = minDist; t < maxDist; t += stepSize) {
+        vec3 p = rayOrigin + rayDir * t;
+        float terrainHeight = heightFunc(p.x, p.z);
+        if (p.y < terrainHeight) {
+            // Interpolate intersection distance
+            resT = t-stepSize+stepSize*(lh-ly)/(p.y-ly-terrainHeight+lh);
+            return true;
+        }
+        // Accuracy proportionate to distance
+        stepSize = 0.01 * t;
+        lh = terrainHeight;
+        ly = p.y;
+    }
+    return false;
+}
+
 void main() {
+    //vec2 uv = gl_FragCoord.xy - 0.5 * Resolution / Resolution.y;
+    vec2 uv = outPos.xy;
+
     fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 }
